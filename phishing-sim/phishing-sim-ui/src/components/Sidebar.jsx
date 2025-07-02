d5d7d4c359e3d18d1288320b8564177e7011f653
hizanrahman <hizanrahman@gmail.com>
2025-07-02 23:59:30 +0530

    Update Sidebar.jsx

--- phishing-sim/phishing-sim-ui/src/components/Sidebar.jsx
+++ phishing-sim/phishing-sim-ui/src/components/Sidebar.jsx
@@ -7,7 +7,7 @@ const Sidebar = () => {
       <nav className="flex flex-col space-y-4">
         <Link to="/dashboard" className="hover:text-blue-300 transition">Dashboard</Link>
         <Link to="/send-email" className="hover:text-blue-300 transition">Send Email</Link>
-        <Link to="/email-template-editor">Email Template Editor</Link>
+        
 
 
       </nav>
