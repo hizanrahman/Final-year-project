b55b1ddad9db1952a1fc66490492c45e74afaede
hizanrahman <hizanrahman@gmail.com>
2025-07-02 23:46:48 +0530

    change

--- phishing-sim/phishing-sim-ui/src/App.js
+++ phishing-sim/phishing-sim-ui/src/App.js
@@ -3,7 +3,7 @@ import SidebarLayout from "./components/SidebarLayout"; // Contains the sidebar
 import Dashboard from "./components/Dashboard";
 import SendEmail from "./components/SendEmail";
 import Login from './components/Login'; 
-import EmailTemplateEditor from './components/EmailTemplateEditor';
+
 
 function App() {
   return (
@@ -16,7 +16,7 @@ function App() {
         <Route element={<SidebarLayout />}>
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/send-email" element={<SendEmail />} />
-          <Route path="/email-template-editor" element={<EmailTemplateEditor />} />
+          
         </Route>
       </Routes>
     </Router>
