ed1b3bf9dd10a98078e86e1f75806a5f4edb389b
hizanrahman <hizanrahman@gmail.com>
2025-07-02 21:59:36 +0530

    Update server.js

--- FINAL_YEAR_PROJECT/FINAL_YEAR_PROJECT/phishing-sim/server.js
+++ FINAL_YEAR_PROJECT/FINAL_YEAR_PROJECT/phishing-sim/server.js
@@ -15,9 +15,11 @@ app.use(express.static("public"));
 app.use(cors());
 app.use(express.json());
 
+
 // Connect to MongoDB
 mongoose
-  .connect("mongodb://localhost:27017/phishingSim", {
+  mongoose
+  .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
