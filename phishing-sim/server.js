e536c9c1cb1c653b5c5f1615c4e63a8d742b5126
hizanrahman <hizanrahman@gmail.com>
2025-07-03 10:17:36 +0530

    Update server.js

--- phishing-sim/server.js
+++ phishing-sim/server.js
@@ -18,10 +18,7 @@ app.use(express.json());
 
 // Connect to MongoDB
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("✅ MongoDB connected"))
   .catch((err) => console.error("❌ MongoDB error:", err));
 
